852f3a12eb093ca3f44e094f5a8b440409c997f3
just Nazar <nazar.mraka@gmail.com>
2024-09-12 11:14:39 +0300

    updated app settings

--- app_setting.py
+++ app_setting.py
@@ -15,6 +15,9 @@ def appSetting():
     while True:
         print('Here you can change the colour of background or text')
         changes = input('To change background type 1, to change text type 2: ')
+        print('Available Colours:')
+        for entry in availavbleColours:
+            print(entry)
         colour = input('Write some colour: ')
 
         isColourAvailable = colour.lower() in availavbleColours
@@ -22,9 +25,11 @@ def appSetting():
         if isColourAvailable:
             if changes == '1':
                 color( bg = colour )
+                print('done')
                 break
             elif changes == '2':
                 color( text = colour )
+                print('done')
                 break
         else:
             print('Try again')
